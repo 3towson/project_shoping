6dc2713abf41ac42220d6b90fbfffb2a04296fa1
3towson <thammaruksa2548@gmail.com>
2024-09-30 21:27:19 +0700

    edit name

--- puma.php
+++ puma.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nike</title>
+    <title>Puma</title>
 </head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
 <link rel="stylesheet" href="vans.css">
